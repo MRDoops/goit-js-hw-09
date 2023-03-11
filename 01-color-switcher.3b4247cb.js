494ccb31dc37be581d09af3a7fe4bd7b5124856e
MRDoops <113108000+MRDoops@users.noreply.github.com>
2023-03-11 12:19:39 +0000

    Deploying to gh-pages from @ MRDoops/goit-js-hw-09@32beb748d53c96974fedc3de8b3c7d3061df594c 🚀

---
+++ 01-color-switcher.3b4247cb.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){d||(d=setInterval(n,1e3),t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",(function(){clearInterval(d),d=null,t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.3b4247cb.js.map
---
+++ 01-color-switcher.ea04456d.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){t||(t=setInterval(a,1e3),e.disabled=!0,n.disabled=!1)})),n.addEventListener("click",(function(){clearInterval(t),t=null,e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.ea04456d.js.map
