32beb748d53c96974fedc3de8b3c7d3061df594c
Dmytro Martynenko <DoopsDM@gmail.com>
2023-03-11 14:19:15 +0200

    wrote random colors

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+startButton.addEventListener('click', onstartButton);
+stopButton.addEventListener('click', onStopColor);
+let intervalId;
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onstartButton() {
+  if (!intervalId) {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }
+}
+
+function onStopColor() {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
